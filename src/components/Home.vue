f7210f5e8d3ff69556717fbc2dc7644af018ae0d
Agniva De Sarker <agnivade@yahoo.co.in>
2017-05-21 14:56:36 +0530

    Adding the loading spinner

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,27 +3,41 @@
     <mu-row gutter>
       <mu-col width="100" tablet="50" desktop="50">
         <mu-sub-header>Latest</mu-sub-header>
-        <mu-content-block v-for="item in latestThreadsDesc" :key="item.id">
-          <mu-list-item :title="item.title">
-            <mu-avatar :src="item.userData.photoURL" slot="leftAvatar"/>
-            <span slot="describe">
-              {{item.lastMessage}}
-            </span>
-          </mu-list-item>
-          <mu-divider />
-        </mu-content-block>
+        <template v-if="showLatestThreads">
+          <mu-content-block v-for="item in latestThreadsDesc" :key="item.id">
+            <mu-list-item :title="item.title">
+              <mu-avatar :src="item.userData.photoURL" slot="leftAvatar"/>
+              <span slot="describe">
+                {{item.lastMessage}}
+              </span>
+            </mu-list-item>
+            <mu-divider />
+          </mu-content-block>
+        </template>
+        <template v-else>
+          <mu-content-block>
+            <mu-circular-progress :size="40"/>
+          </mu-content-block>
+        </template>
       </mu-col>
       <mu-col width="100" tablet="50" desktop="50">
         <mu-sub-header>Top</mu-sub-header>
-        <mu-content-block v-for="item in topThreadsDesc" :key="item.id">
-          <mu-list-item :title="item.title">
-            <mu-avatar :src="item.userData.photoURL" slot="leftAvatar"/>
-            <span slot="describe">
-              {{item.lastMessage}}
-            </span>
-          </mu-list-item>
-          <mu-divider />
-        </mu-content-block>
+        <template v-if="showTopThreads">
+          <mu-content-block v-for="item in topThreadsDesc" :key="item.id">
+            <mu-list-item :title="item.title">
+              <mu-avatar :src="item.userData.photoURL" slot="leftAvatar"/>
+              <span slot="describe">
+                {{item.lastMessage}}
+              </span>
+            </mu-list-item>
+            <mu-divider />
+          </mu-content-block>
+        </template>
+        <template v-else>
+          <mu-content-block>
+            <mu-circular-progress :size="40"/>
+          </mu-content-block>
+        </template>
       </mu-col>
     </mu-row>
 
@@ -68,6 +82,12 @@ export default {
     })
   },
   computed: {
+    showLatestThreads () {
+      return this.latestThreads.length !== 0
+    },
+    showTopThreads () {
+      return this.topThreads.length !== 0
+    },
     latestThreadsDesc () {
       return this.latestThreads.reverse()
     },
